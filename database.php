2bbe5f38414b8c9ce2bf281ce7111bf9202c76a8
Aditya <aprataps710@gmail.com>
2024-09-30 05:20:45 +0530

    database file updated

--- database.php
+++ database.php
@@ -1,11 +1,12 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 $servername = $_ENV['DB_SERVER'];
 $username = $_ENV['DB_USERNAME']; 
-$password = $_ENV['DB_DATABASE']; 
-$database = $_ENV['DB_SERVER'];
+$password = $_ENV['DB_PASSWORD']; 
+$database = $_ENV['DB_DATABASE'];
 $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
