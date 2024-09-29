d9f41246b21673ef549f064e685ac7f6797c3101
Aditya <aprataps710@gmail.com>
2024-09-30 05:08:39 +0530

    database changes

--- database.php
+++ database.php
@@ -1,8 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root"; 
-$password = ""; 
-$database = "Invoice";
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+$servername = $_ENV['DB_SERVER'];
+$username = $_ENV['DB_USERNAME']; 
+$password = $_ENV['DB_DATABASE']; 
+$database = $_ENV['DB_SERVER'];
 $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
